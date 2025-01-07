ac56db96625ec1bc19a6ef00eb19259a9f9d63f6
Fabricio Laboriel <fabriciolaboriel@gmail.com>
2025-01-07 08:40:33 -0700

    wrote code for saved candidates, candidateSearch and candidate.interface

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,30 @@
+import { NavLink } from 'react-router-dom';
+
 const Nav = () => {
+
   // TODO: Add necessary code to display the navigation bar and link between the pages
   return (
-    <div>Nav</div>
+    
+      <ul className="nav">
+        <li className="nav-item">
+          <NavLink
+          to="/"
+          end
+          className={({ isActive }) => (isActive ? 'nav-link-active' : 'nav-link')}
+          >
+            Home
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink
+            to="/SavedCandidates"
+            className={({ isActive }) => (isActive ? 'nav-link-active' : 'nav-link')}
+            >
+              Potential Candidates
+            </NavLink>
+        </li>
+      </ul>
+    
   )
 };
 
