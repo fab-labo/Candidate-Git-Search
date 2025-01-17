e29d3bbe188ed0a02084925c6f3391bbf0b537d0
Fabricio Laboriel <fabriciolaboriel@gmail.com>
2025-01-16 20:06:05 -0700

    final commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  envDir: './env',
-  plugins: [react()],
+import dotenv from 'dotenv';
+dotenv.config();
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    envDir: './env',
+    plugins: [react()],
+    define: {
+      'process.env': env
+    }
+  };
 });
